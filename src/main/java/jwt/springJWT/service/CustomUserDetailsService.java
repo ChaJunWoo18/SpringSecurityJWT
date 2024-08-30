1d5e91e56d1b8b5da7e9c1ce7d0038198838492e
ChaJunWoo18 <wnsdnxla@naver.com>
2024-08-31 02:28:05 +0900

    jwt실습(cors 없음)

---
+++ src/main/java/jwt/springJWT/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package jwt.springJWT.service;
+
+import jwt.springJWT.entity.UserEntity;
+import jwt.springJWT.jwt.CustomUserDetails;
+import jwt.springJWT.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //DB에서 조회
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
